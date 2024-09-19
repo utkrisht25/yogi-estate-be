92327cffc42545fbb19dedf12b7587a0308c7313
utkrisht25 <utkrishtyogi@gmail.com>
2024-09-19 23:28:38 +0530

    CORS Verification

--- api/index.js
+++ api/index.js
@@ -26,7 +26,10 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://utkrisht25.github.io',
+  credentials: true
+}));
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
@@ -46,6 +49,9 @@ app.get('*', (req, res) => {
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
+  
+  res.setHeader('Access-Control-Allow-Origin', 'https://utkrisht25.github.io');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   return res.status(statusCode).json({
     success: false,
     statusCode,
